927706aeaaa63a9dd6f71f6b6c930ebc669b5b39
Whit Stroup <whit@stroups.net>
2021-02-10 23:19:47 -0600

    working on Marvel CSV demo

---
+++ CSVDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CSVDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var reader = new StreamReader("Marvel.csv"))
+            {
+                List<string> myFile = new List<string>();
+
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+
+
+                    myFile.Add(line);
+
+                }
+                foreach (var item in myFile)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+               
+        }
+    }
+}
